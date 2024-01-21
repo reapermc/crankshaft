89d2136f2bfa38887fe84310e53e78432adaec1a
Amy Opas <amy.opas@proton.me>
2024-01-21 12:52:11 +0100

    add docs ^^

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'crankshaft'
-copyright = '2024, ArcticYeti'
-author = 'ArcticYeti'
-release = '1.0.0'
+project = "crankshaft"
+copyright = "2024, ArcticYeti"
+author = "ArcticYeti"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -18,21 +18,21 @@ extensions = [
     "sphinx_inline_tabs",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_title = 'Crankshaft Documentation'
-html_logo = 'logo.png'
-html_static_path = ['_static']
+html_theme = "furo"
+html_title = "Crankshaft Documentation"
+html_logo = "logo.png"
+html_static_path = ["_static"]
 html_theme_options = {
     "dark_css_variables": {
-        "color-brand-primary": '#EC7210',
-        "color-brand-content": '#EC7210',
+        "color-brand-primary": "#EC7210",
+        "color-brand-content": "#EC7210",
     },
     "sidebar_hide_name": True,
 }
